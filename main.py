1d72207749d3b47df39e94763aec7c3017d10a79
Virendraverma9950 <153084063+Virendraverma9950@users.noreply.github.com>
2024-03-30 09:38:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6535968101:AAFO4BnJlqGM5-EZ_h4XO7YXufbjsdq1JQw",
-             api_id= 20200825,
-             api_hash= "7e7f93e266c390d9cc5c2bffbe921e36")
+             bot_token= "6846325385:AAGF5EQTa54YpQdBe21op8RCwi6WJMz96JQ",
+             api_id= 25919081,
+             api_hash= "0bc2fdba14b16b44f0d89729ed8d2118")
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
